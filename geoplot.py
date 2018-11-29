06241f8c4f245e983efc35c2bdaa6f7426185413
Nathan Bourne <nbourne22@gmail.com>
2018-11-29 13:54:54 +0000

    Change to geoplot and new LST_SNR_analysis
    Change to geoplot: raster works correctly; have added functionality
    to pass 'Norm' using matplotlib.colors normalization classes,
    as an alternative to passing data to raster_utils.rescale_image
    Colorbar updated accordingly.
    
     On branch master
     Your branch is up to date with 'origin/master'.
    
     Changes to be committed:
    	new file:   .ipynb_checkpoints/LST_SNR_analysis-checkpoint.ipynb
    	modified:   ECO_LST_Comparison.ipynb
    	new file:   LST_SNR_analysis.ipynb
    	modified:   __pycache__/geoplot.cpython-36.pyc
    	modified:   geoplot.py

--- geoplot.py
+++ geoplot.py
@@ -14,7 +14,10 @@ continuous_to_discrete():   Convert an input *continuous* data array into a disc
 import matplotlib
 import matplotlib.pyplot as plt
 import pandas as pd
-import numpy as np
+from rasterio import plot as rplt
+import numpy.ma
+
+import raster_utils as ru
 
 def make_figure(shape=1,figsize=None):
     """
@@ -190,59 +193,110 @@ def plot_by_date(axes, gdf, datecolname,
 
     return base,(vminf,vmaxf)
 
-def raster():
+def raster(axes, im, 
+           transform=None,
+           colormap=None,
+           vmin=None,
+           vmax=None,
+           pmin=0,
+           pmax=100,
+           Norm=None,
+           rescale_kind='',
+           title='',
+           no_axes_ticks=True,
+           use_rst_plot=True,
+           titlesize=15,
+           **clahe_equalize_kwargs
+    ):
     """
     Display image of raster data
     
     Input:
-    im/src: either image array or rasterio dataset?
-    axes:
-    colormap:
-    vmin, vmax:
-    normalise method or matplotlib Norm object?:
-    Affine transform describing x,y axes of the image?
+    axes = matplotlib.axes.Axes object
+    im = either image array  [or rasterio dataset??]
+    transform = optional Affine transform describing x,y axes of the image
+    colormap: matplotlib colormap used for encoding values of data in colname
+              some useful examples include:
+              - afmhot
+              - gray/Greys
+              - viridis
+              - inferno2
+              - remember append "_r" to reverse the colormap
+              https://matplotlib.org/examples/color/colormaps_reference.html
+    vmin,vmax = min,max values assigned to 0,255 in the colormap
+    pmin,pmax = percentiles used to set vmin,vmax if the latter are None
+    rescale_kind = string to pass to raster_utils.rescale_image as 'kind'
+        Note that using rescale_kind, the colorbar may not be labelled 
+        correctly, hence it is better to pass Norm
+    Norm = an instance of one of the normalization classes from 
+        matplotlib.colors, as an alternative to passing rescale_kind
+        If neither of these are passed then a simple linear normalization
+        between vmin and vmax is used
+
+    use_rst_plot = whether to use rasterio plot (otherwise just pyplot.imshow)
+    
 
     Return:
     the axes object containing the data (return value of rasterio.plot.show
         or pyplot.imshow)
-    
+    the Normalize instance of the axes
 
     """
 
     # PROCESS:
 
     # 1. clean input (NaN, None, numpy.mask)
-    # Nans = np.array(~np.isfinite(inputdata))
-    # Nones = np.array(inputdata is None)
-    # data = ma.array(inputdata,mask=(Nans | Nones))
+    data = np.squeeze(im).copy()
+    Nans = np.array(~np.isfinite(data))
+    Nones = np.array(data is None)
+    data = np.ma.array(data,mask=(Nans | Nones)).filled()
 
     # 2. determine vmin/vmax, eg from input or percentiles
-    if pmin>=0 and pmax<=100:
-        vmin,vmax = np.nanpercentile(data,[pmin,pmax])
+    if vmin is None:
+        vmin = np.nanpercentile(data,pmin)
+    if vmax is None:
+        vmax = np.nanpercentile(data,pmax)
+
+    data = np.where(data>=vmin,data,vmin)
+    data = np.where(data<=vmax,data,vmax)
 
     # 3. normalise data
-    # data = ru.rescale_image(inputdata,kind='linear')
+    # There are three options:
+    # If rescale_kind is passed as a string, rescale_image will be used
+    # Otherwise, can use matplotlib.colors to normalize the data - the
+    # Norm instance can be passed as an input 
+    # If neither is passed then a simple linear normalization is used
+    if len(rescale_kind)>0:
+        data = ru.rescale_image(data,kind=rescale_kind,
+                                **clahe_equalize_kwargs)
+    #
+    if Norm is None:
+        Norm = matplotlib.colors.Normalize(vmin=vmin,vmax=vmax)
 
     # 4. display
     if use_rst_plot:
-        base = rplt.show(data,ax=axes,vmin=vmin,vmax=vmax,cmap=colormap,
-                  title=title)
+        base = rplt.show(data,ax=axes,cmap=colormap,#vmin=vmin,vmax=vmax,
+                  title=title,transform=transform,
+                  norm=Norm
+        )
     else:
-        base = axes.imshow(data,vmin=vmin,vmax=vmax,cmap=colormap)
-
+        base = axes.imshow(data,cmap=colormap,vmin=vmin,vmax=vmax,
+                           norm=Norm)
+        # base = axes.pcolormesh(data,norm=Norm,cmap=colormap)
+        # Norm = vmin,vmax
 
     # 5. set ticks, labels and title
-    axes.set_title(title)
+    axes.set_title(title,size=titlesize)
     
     if no_axes_ticks:
         axes.set_xticks([])
         axes.set_yticks([])
 
     # 6. return
-    return base
+    return base,Norm #(vmin,vmax)
 
 
-def colorbar(axes,vminmax,
+def colorbar(axes,Norm,
         colormap=None,
         discrete=False,
         disc_bounds=[],
@@ -258,8 +312,8 @@ def colorbar(axes,vminmax,
     
     Input:
     axes = matplotlib.axes.Axes object
-    vminmax = tuple (vmin,vmax) of data plotted in axes
-
+    Norm = an instance of one of the Normalize classes in matplotlib.colors
+    OR Norm = tuple (vmin,vmax) of data plotted in axes (assumed linear scale)
 
     - has to be able to deal with dates as well as float data
     - should be able to specify ticks and ticklabels
@@ -276,22 +330,24 @@ def colorbar(axes,vminmax,
 
     # 1. position the colorbar
     #cbpos = [0.05,0.95,0.9,0.02]
-    if locbottom:
-        cbpos = [axes.get_position().xmin,0.01,axes.get_position().width,0.02]
+    if (type(axes)==matplotlib.image.AxesImage) or (type(axes)==matplotlib.collections.QuadMesh):
+        Aaxes=axes.axes
     else:
-        cbpos = [axes.get_position().xmin,0.97,axes.get_position().width,0.02]
+        Aaxes=axes
 
+    if locbottom:
+        cbpos = [Aaxes.get_position().xmin,0.01,Aaxes.get_position().width,0.02]
+    else:
+        cbpos = [Aaxes.get_position().xmin,0.97,Aaxes.get_position().width,0.02]
 
-    
-    # adjust alignment of subplots...
-    # fig.subplots_adjust(left=0.05,right=0.95,
-    #                     bottom=0.05,top=1,
-    #                     wspace=0.05) 
-
- 
 
     # 2. determine vmin, vmax 
-    vmin,vmax = vminmax
+    # vmin,vmax = vminmax
+
+    # Ensure backwards compatibility with version where vmin,vmax were passed
+    if type(Norm)==tuple:
+        vmin,vmax = Norm
+        Norm = plt.Normalize(vmin=vmin,vmax=vmax)
 
     # 3. define discrete bounds if required 
 
@@ -303,19 +359,23 @@ def colorbar(axes,vminmax,
         CB0 = matplotlib.colorbar.ColorbarBase(cax,orientation='horizontal',
                                               boundaries= disc_bounds,
                                               cmap=colormap,
-                                              norm=plt.Normalize(vmin=vmin, vmax=vmax)
+                                              norm=Norm
+                                              # norm=plt.Normalize(vmin=vmin, vmax=vmax)
                                               )
         CB0.set_ticks(disc_tickvals)
         CB0.set_ticklabels(disc_ticklabs)
     else:
         sm = plt.cm.ScalarMappable(cmap=colormap, 
-                               norm=plt.Normalize(vmin=vmin, vmax=vmax))
+                               norm=Norm
+                               # norm=plt.Normalize(vmin=vmin, vmax=vmax)
+                               )
         sm._A = []
         CB0 = fig.colorbar(sm, orientation='horizontal', cax=cax)
-
+        
         # 5. set ticks, labels 
-        cax.xaxis.set_major_locator(plt.MaxNLocator(ticks+1))
         if isdate:
+            # Beware of the following line when using any Norm other than linear Normalize()
+            cax.xaxis.set_major_locator(plt.MaxNLocator(ticks+1))
             cax_date_labels = pd.to_datetime(cax.xaxis.get_ticklocs()).strftime('%Y-%m-%d')
             cax.xaxis.set_ticklabels(cax_date_labels)
 
